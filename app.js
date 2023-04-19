e527b190039143fa94f866bfb267dcadbc0ac9f8
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-18 20:24:38 -0400

    function prompt for intern info

--- app.js
+++ app.js
@@ -68,3 +68,37 @@ const promptManager = async () => {
     };
   };
   
+  const promptIntern = async () => {
+    const internQuestions = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the intern\'s name?',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'What is the intern\'s employee ID?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is the intern\'s email address?',
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: 'What is the intern\'s school name?',
+      },
+    ];
+  
+    const answers = await inquirer.prompt(internQuestions);
+  
+    return {
+      name: answers.name,
+      id: answers.id,
+      email: answers.email,
+      school: answers.school,
+    };
+  };
+  
