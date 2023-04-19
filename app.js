9c11d6b8e7ddc5e2223560cb977bc9adafb8e60f
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-18 20:21:27 -0400

    function to prompt for an engineer's information

--- app.js
+++ app.js
@@ -33,3 +33,38 @@ const promptManager = async () => {
       officeNumber: answers.officeNumber,
     };
   };
+
+  const promptEngineer = async () => {
+    const engineerQuestions = [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the engineer\'s name?',
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: 'What is the engineer\'s employee ID?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is the engineer\'s email address?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is the engineer\'s GitHub username?',
+      },
+    ];
+  
+    const answers = await inquirer.prompt(engineerQuestions);
+  
+    return {
+      name: answers.name,
+      id: answers.id,
+      email: answers.email,
+      github: answers.github,
+    };
+  };
+  
