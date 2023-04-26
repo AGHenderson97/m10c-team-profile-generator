ae60d15acbebf1d94bf54ab9e68834fc88d07598
AGHenderson97 <agh.pe.gatech@outlook.com>
2023-04-26 19:39:23 -0400

    corrected code for generateMarkdown.js

--- src/generateMarkdown.js
+++ src/generateMarkdown.js
@@ -1,53 +1,63 @@
-function generateMarkdown(data) {
-    return `# ${data.title}
+function generateMarkdown(team) {
+  const html = `
+  <!DOCTYPE html>
+  <html lang="en">
   
-  ## Description
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Profile</title>
+    <link rel="stylesheet" href="./style.css">
+  </head>
   
-  This is a command-line application that dynamically generates a professional README.md file from a user's input using the Inquirer package. The generated README file includes information on the app's purpose, how to use it, how to install it, how to report issues, and how to make contributions. 
+  <body>
+    <header>
+      <h1>My Team</h1>
+    </header>
+    <main>
+      ${generateCards(team)}
+    </main>
+  </body>
   
-  ## Table of Contents
-  
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [License](#license)
-  - [Contributing](#contributing)
-  - [Tests](#tests)
-  - [Questions](#questions)
-  
-  ## Installation
-  
-  To install the necessary dependencies and run the application, use the following commands:
-  
-  \`\`\`
-  npm install
-  node app.js
-  \`\`\`
-  
-  ## Usage
-  
-  After running the command above, follow the prompts to enter information about your application repository. Once you've completed all the prompts, a high-quality README.md file will be generated in the root directory of your project.
-  
-  ## License
-  
-  This application is licensed under the MIT license.
-  
-  ## Contributing
-  
-  Contributions to this project are welcome! To contribute, please fork this repository and create a new pull request.
-  
-  ## Tests
-  
-  To test the application, run the following command:
-  
-  \`\`\`
-  npm test
-  \`\`\`
-  
-  ## Questions
-  
-  If you have any questions about the repository, open an issue or contact me directly at agh.pe.gatech@outlook.com. You can find more of my work at https://github.com/AGHenderson97.
+  </html>
   `;
+
+  return html;
+}
+
+function generateCards(team) {
+  let cards = "";
+  for (let i = 0; i < team.length; i++) {
+    cards += `
+    <section>
+      <h2>${team[i].name}</h2>
+      <h3>${team[i].getRole()}</h3>
+      <ul>
+        <li>ID: ${team[i].id}</li>
+        <li>Email: <a href="mailto:${team[i].email}">${team[i].email}</a></li>
+      `;
+    if (team[i].getRole() === "Manager") {
+      cards += `
+        <li>Office Number: ${team[i].officeNumber}</li>
+        `;
+    } else if (team[i].getRole() === "Engineer") {
+      cards += `
+        <li>Github: <a href="https://github.com/${team[i].github}" target="_blank">${team[i].github}</a></li>
+        `;
+    } else if (team[i].getRole() === "Intern") {
+      cards += `
+        <li>School: ${team[i].school}</li>
+        `;
+    }
+    cards += `
+      </ul>
+    </section>
+    `;
   }
-  
-  module.exports = generateMarkdown;
+  return cards;
+}
+
+module.exports = generateMarkdown;
+
   
